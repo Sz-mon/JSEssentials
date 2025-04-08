a4596059dd40275a85b9f16cfa67b63f7234db75
Szymon <szymon.alachamowicz@akersolutions.com>
2025-04-08 01:29:55 -0400

    scoped variables added

---
+++ Scope/scope_lab.js
@@ -0,0 +1,27 @@
+// Global scope
+var globalVar = "I'm global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    //Block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
+//console.log(blockVar);
+//console.log(blockLet);
+
+function show(){
+    var functionVar = "block-scoped var";
+    let functionLet = "block-scoped let";
+    const functionConst = "block-scoped const";
+}
+
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
